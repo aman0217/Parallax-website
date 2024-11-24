324a8865bc52789db336e929392aad5803ad3a4c
aman0217 <ithakur021751@gmail.com>
2024-11-24 14:00:27 +0530

    index.js
    $ git add index.html about.html index.css index.js
    $ git commit -m "Add a JavaScript page"
    $ git push origin main

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function submitForm() {
         feedback.textContent = "Please fill out field.";
         feedback.style.color = "red";
     } else {
-        feedback.textContent = "Thank you, " + username + "! Welcome to this page:" + about.html ;
+        feedback.textContent = "Thank you, " + username + "! Welcome to this page:";
         feedback.style.color = "green";
     }
 }
